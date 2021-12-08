e4dbb81230e3ce60df93c14a5a0e1f931bec58fe
kerlq <oussamagaming2001@outlook.de>
2021-12-08 19:36:41 +0100

    Add register and login system and add global methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+    def login
+
+    end
+    
+    def create
+        @user = User.find_by(username: params[:username])
+        if !!@user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path, notice: "Invalid Login!"
+        end
+        
+    end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to users_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  get '/register', to: 'users#new'
+  
+  get '/logout', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
+  resources :users
 end
