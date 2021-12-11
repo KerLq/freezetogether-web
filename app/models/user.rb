cadbad5cde6478386667953577c9e95e103742b9
kerlq <oussamagaming2001@outlook.de>
2021-12-11 18:10:17 +0100

    Username has max length of 16 chars and password has min length of 8 chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates_length_of :username, maximum: 16
+    validates_length_of :password, minimum: 8 
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
