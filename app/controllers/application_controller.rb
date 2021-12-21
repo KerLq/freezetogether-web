a1f9773730c5a6ece286534e9d8e8f9cf424c4f8
kerlq <oussamagaming2001@outlook.de>
2021-12-21 22:19:32 +0100

    Add is_admin and is_current_user global methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,14 @@ class ApplicationController < ActionController::Base
     def logged_in?    
         !!current_user
     end
+    
+    def is_admin?
+        logged_in? ? current_user.admin? : false
+    end
+    
+    def is_current_user(user)
+        if logged_in?
+            current_user == user ? true : false
+        end
+    end
 end
--- app/controllers/backend/backend_controller.rb
+++ app/controllers/backend/backend_controller.rb
@@ -1,6 +1,7 @@
 class Backend::BackendController < ApplicationController
     #layout 'backend'
     before_action :is_admin?
+    
     def is_admin?
         if !current_user.nil?
             if !current_user.admin?
