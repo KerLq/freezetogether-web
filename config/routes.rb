1484260779a4724bbf04361221321a056e4b6c1d
Dogus <dogus92009@live.de>
2022-02-13 13:03:17 +0100

    clean up (& fix)

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
-    
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/login', to: 'frontend/sessions#login'
   post '/login', to: 'frontend/sessions#create'
   get '/register', to: 'frontend/users#new'
   post '/register', to: 'frontend/users#create'
-  
+
   get '/logout', to: 'frontend/sessions#destroy'
   post '/logout', to: 'frontend/sessions#destroy'
-  
+
   scope module: 'frontend', as: 'frontend' do
     root to: "welcome#index"
     resources :users, only: [:show, :edit]
     resources :sessions
     resources :news
   end
-  
-    scope module: 'backend', as: 'backend', path: 'admin' do
-      root to: "welcome#index"
-      resources :users
-      resources :news
-    end
+
+  scope module: 'backend', as: 'backend', path: 'admin' do
+    root to: "welcome#index"
+    resources :users
+    resources :news
+  end
 end
