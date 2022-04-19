068abf211316ba489647c506ba6b33965f709c64
Dogus <dogus92009@live.de>
2022-04-19 18:31:46 +0200

    upload profile avatar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,9 @@
+//= require jquery
 //= require components/PureSnow
+//= require components/avatar-upload
+
+// KAPPUT (?)
+//import "trix"
+//import "@rails/actiontext"
+//require("libraries/actiontext@6.1.4")
+//require("libraries/trix@1.3.1")
---
+++ app/assets/javascripts/components/avatar-upload.js
@@ -0,0 +1,12 @@
+const avatarProfile = document.querySelector(".profile-avatar");
+const hoverChangeAvatar = avatarProfile.querySelector(".avatar-upload");
+const avatarChangeForm = avatarProfile.querySelector("form.edit_user");
+const avatarFileUpload = avatarChangeForm.querySelector("input#user_avatar");
+
+hoverChangeAvatar.onclick = function() {
+  avatarFileUpload.click();
+};
+
+avatarFileUpload.onchange = function() {
+  avatarChangeForm.submit();
+};
