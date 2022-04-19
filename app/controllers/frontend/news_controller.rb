cbd667e5032972da7513dd51e913a58f0d657ec4
kerlq <oussamagaming2001@outlook.de>
2022-04-19 17:38:14 +0200

    Design news show page

--- app/controllers/frontend/news_controller.rb
+++ app/controllers/frontend/news_controller.rb
@@ -8,6 +8,8 @@ class Frontend::NewsController < Frontend::FrontendController
   # GET /news/1 or /news/1.json
   def show
     @news = News.find(params[:id])
+    @other_news = News.limit(3).order("RANDOM()")
+
   end
   
 end
