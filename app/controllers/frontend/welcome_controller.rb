41f6e9082beabdfa8f2d807c317aec38b1d69604
kerlq <oussamagaming2001@outlook.de>
2022-04-19 16:19:53 +0200

    Latest 3 news get displayed on landingpage

--- app/controllers/frontend/welcome_controller.rb
+++ app/controllers/frontend/welcome_controller.rb
@@ -1,5 +1,5 @@
 class Frontend::WelcomeController < Frontend::FrontendController
     def index
-        
+        @news = News.last(3)
     end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "trix"
+
 import "@rails/actiontext"
+import "trix"
 
 Rails.start()
 Turbolinks.start()
