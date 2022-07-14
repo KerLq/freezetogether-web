50588d6989785f8fda7e42c002dde9a40d1ff732
kerlq <oussamagaming2001@outlook.de>
2022-07-14 15:04:37 +0200

    Add token column to user and jwt token gets saved

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -6,14 +6,22 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+      time = Time.now + 10.seconds.to_i
+      @user.update(token: token)
+      render json: { token: @user.token, exp: time.strftime("%m-%d-%Y %H:%M"),
                      username: @user.username }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
   end
 
+  def validate_token
+    if @user = User.find_by(token: request.headers['Authorization'])
+      render json: { token: @user.token }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
   private
 
   def login_params
