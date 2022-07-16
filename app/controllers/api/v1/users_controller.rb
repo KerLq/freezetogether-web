ccffd1420165ca048637afc9146dff8089f9fb42
kerlq <oussamagaming2001@outlook.de>
2022-07-16 12:57:40 +0200

    Catch verification error exception and add 'me' api to receive its user data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::UsersController < Api::V1::ApiController
 
+	def me
+		render json: @current_user, status: :ok
+	end
+
   private
   def user_params
     params.require(:user).permit(
