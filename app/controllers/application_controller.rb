392d52860d881c42e0e59f60f01f8da218ce43d6
KerLq <kerlqdev@outlook.de>
2022-09-25 15:09:10 +0200

    Add ActiveSupport CurrentAttrbiutes representing user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,14 @@ class ApplicationController < ActionController::Base
   include self::PunditInit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  def current_user
-    User.find(session[:user_id]) if session[:user_id]
+  before_action do
+    next unless session[:user_id]
+
+    Current.user = User.find_by(id: session[:user_id])
   end
 
   def user_not_authorized
-    fallback_location = current_user ? frontend_user_path(current_user) : frontend_root_path
+    fallback_location = Current.user ? frontend_user_path(Current.user) : frontend_root_path
     redirect_back(fallback_location:, flash: { error: t('errors.not_authorized') })
   end
 end
