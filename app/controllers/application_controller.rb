d2aaaa46dceacafa8c20154033bc9edfe1840de4
KerLq <kerlqdev@outlook.de>
2022-09-25 15:05:42 +0200

    Refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,6 @@ class ApplicationController < ActionController::Base
   include self::PunditInit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  helper_method :current_user
-  helper_method :logged_in?
-  helper_method :admin?
-  helper_method :current_user?
-
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
@@ -16,17 +11,5 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     fallback_location = current_user ? frontend_user_path(current_user) : frontend_root_path
     redirect_back(fallback_location:, flash: { error: t('errors.not_authorized') })
-  end
-
-  def logged_in?
-    current_user
-  end
-
-  def admin?
-    logged_in? ? current_user.admin? : false
-  end
-
-  def current_user?(user)
-    current_user == user if logged_in?
   end
 end
