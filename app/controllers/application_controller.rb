9afb05aab7417fef29469f60b6d9a46f6bfd6ecf
KerLq <kerlqdev@outlook.de>
2022-09-25 14:02:25 +0200

    Add pundit authorization in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+	include Pundit::Authorization
+
   helper_method :current_user
   helper_method :logged_in?
   helper_method :admin?
