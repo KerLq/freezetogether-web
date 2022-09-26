926b1f54dca22b9c25e6cc96003da2469b01c855
KerLq <kerlqdev@outlook.de>
2022-09-26 21:53:12 +0200

    Add active storage validation gem and validate image

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,6 +4,7 @@ class Character < ApplicationRecord
   validates :title, presence: true
   validates :name, presence: true
   validates :description, presence: true
+  validates :image, attached: true, content_type: ['image/png', 'image/jpeg', 'image/svg']
 
   has_one_attached :image
   has_many :character_perks, dependent: :delete_all
