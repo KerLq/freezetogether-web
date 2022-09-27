db27575d45c408c57c7cb3cd551ca8067516cb37
KerLq <kerlqdev@outlook.de>
2022-09-27 17:40:22 +0200

    Remove trix from backend css, add user login helper for rspec, write rspec test for character creation and add rspec execution in CI

---
+++ spec/support/login.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+def login_user(**user_params)
+  user = create(:user, password: 'BenSpencer', email_confirmed: true, **user_params)
+
+  visit login_path
+
+  fill_in 'email', with: user.email
+  fill_in 'password', with: user.password
+  click_button 'Login'
+  expect(page).to have_text('Successfully logged in!')
+
+  user
+end
