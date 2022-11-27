e0d60098507c1a89606ca63c6436032319f1c92f
Dogus <dogus92009@live.de>
2022-11-27 14:10:00 +0100

    navigation active state

--- app/helpers/backend_helper.rb
+++ app/helpers/backend_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module BackendHelper
+  def active_link_to(name = nil, options = nil, html_options = nil, &block)
+    active_class = html_options[:active] || "active"
+    html_options.delete(:active)
+    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options) || request.path =~ (/#{options}/) && html_options[:affinity]
+    link_to(name, options, html_options, &block)
+  end
 end
