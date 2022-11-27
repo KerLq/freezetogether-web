df170eb2ab0ca966eadea500aca0b8c6afb4afb8
Dogus <dogus92009@live.de>
2022-11-27 16:28:23 +0100

    navigation link active state

--- app/helpers/backend_helper.rb
+++ app/helpers/backend_helper.rb
@@ -4,7 +4,7 @@ module BackendHelper
   def active_link_to(name = nil, options = nil, html_options = nil, &block)
     active_class = html_options[:active] || "active"
     html_options.delete(:active)
-    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options) || request.path =~ (/#{options}/) && html_options[:affinity]
+    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options) || request.path =~ (/#{options}/) && !html_options[:disable_affinity]
     link_to(name, options, html_options, &block)
   end
 end
