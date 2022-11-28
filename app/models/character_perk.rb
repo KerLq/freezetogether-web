b0258a2ea7f564cc2a5fa21f0297f0f93d6df9f0
Dogus <dogus92009@live.de>
2022-11-28 21:03:49 +0100

    remove perks

--- app/models/character_perk.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CharacterPerk < ApplicationRecord
-  belongs_to :perk
-  belongs_to :character
-  delegate :name, :image, to: :perk
-end
--- app/models/perk.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Perk < ApplicationRecord
-  has_one_attached :image
-  has_many :character_perks, dependent: :delete_all
-end
