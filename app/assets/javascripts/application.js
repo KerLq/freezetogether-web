5e10482c997d44db50b50a8aa397065276739856
Dogus <dogus92009@live.de>
2022-11-28 18:55:45 +0100

    actiontext without webpacker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,3 @@
 //= require components/avatar-upload
 //= require components/tabs
 
-// KAPPUT (?)
-//import "trix"
-//import "@rails/actiontext"
-//require("libraries/actiontext@6.1.4")
-//require("libraries/trix@1.3.1")
