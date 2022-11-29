6c81d527cee34f7c84c5d68980a186ddf40e2e0b
Dogus Dolu <dogus92009@live.de>
2022-11-29 15:25:08 +0100

    dependencies

--- app/helpers/backend_helper.rb
+++ app/helpers/backend_helper.rb
@@ -2,9 +2,12 @@
 
 module BackendHelper
   def active_link_to(name = nil, options = nil, html_options = nil, &block)
-    active_class = html_options[:active] || "active"
+    active_class         = html_options[:active] || 'active'
     html_options.delete(:active)
-    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options) || request.path =~ (/#{options}/) && !html_options[:disable_affinity]
+    if current_page?(options) || (request.path =~ (/#{options}/) && !html_options[:disable_affinity])
+      html_options[:class] =
+        "#{html_options[:class]} #{active_class}"
+    end
     link_to(name, options, html_options, &block)
   end
 end
