9b1f61cf92803d574c971ea293d15e778e361ff9
Dogus <dogus92009@live.de>
2022-11-29 18:54:45 +0100

    dependencies

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,7 +11,7 @@ require 'rspec/rails'
 require 'support/factory_bot'
 require 'support/capybara'
 
-require "action_text/system_test_helper"
+require 'action_text/system_test_helper'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
