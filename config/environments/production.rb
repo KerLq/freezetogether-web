f4b5f530fbabb7973cc5eb5b1737f0d36cb38893
Dogus <dogus92009@live.de>
2022-12-13 23:28:21 +0100

    env hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
   # SMTP settings
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method       = :smtp
-  config.action_mailer.default_url_options   = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options   = { host: 'freeze-together.xyz' }
   config.action_mailer.perform_deliveries    = true
 
   # SMTP settings
