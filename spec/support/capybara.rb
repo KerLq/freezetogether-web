5de707eb66547b5c2a98c312cc9e7d8097f6dbf5
Dogus <dogus92009@live.de>
2022-12-13 19:39:49 +0100

    remove redundant ::

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,7 +6,7 @@
 Capybara.server = :puma, { Silent: true }
 
 Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
+  options = Selenium::WebDriver::Chrome::Options.new
 
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
