ada76528b50113119bacf630d05960181ee9c485
Dogus <dogus92009@live.de>
2023-01-10 18:50:37 +0100

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get '/logout', to: 'frontend/sessions#destroy'
   post '/logout', to: 'frontend/sessions#destroy'
 
-  get 'download_game', to: "frontend/welcome#download_game"
+  get 'download_game', to: 'frontend/welcome#download_game'
 
   namespace :api do
     namespace :v1 do
