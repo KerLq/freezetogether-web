f279e3cb0f064d0c7d6fb2502b85515e01bb0814
kerlq <kerlqdev@outlook.de>
2023-01-19 17:22:10 +0100

    Revert changes to find bug

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -7,18 +7,18 @@ module Api
         json_decoded = JSON.parse(params[:game])
 
         achievements = json_decoded['game']['achievements'].collect do |achievement|
-          Achievement.find_by(title: achievement['title'], scores: achievement['score'])
+          Achievement.find_by(title: achievement['title'], scores: achievement['scores'])
         end
 
         coins        = json_decoded['game']['coins'].to_i
         score        = json_decoded['game']['score'].to_i
         milliseconds = json_decoded['game']['time'].to_f
 
-        time = '00:00:00:00'
-
         if milliseconds.present?
           seconds = milliseconds / 1000
           time    = Time.at(seconds).utc.strftime('%H:%M:%S:%L')
+        else
+          time = 0
         end
 
         game  = Current.user.games.build(score:, coin: coins, time:) # Rename coin column to 'coins'
